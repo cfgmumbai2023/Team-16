5f133259aca7f896c256e38ccdf80a8e0119023a
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2023-07-09 01:08:31 +0530

    Teacher endpoint added

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./admin/routes/authRoutes');
 const studentRoutes = require('./admin/routes/studentRoutes')
+const teacherRoutes = require('./admin/routes/teacherRoutes')
 
 app.use(express.json());
 app.use('/login', authRoutes);
 app.use('/student', studentRoutes);
+app.use('/teacher', teacherRoutes);
 
 const PORT = 8080;
 app.listen(PORT, () => {
