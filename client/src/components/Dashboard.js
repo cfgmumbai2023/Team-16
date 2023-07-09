ca72eca6e335e529322db2e707b3bbc7593599b7
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2023-07-09 06:51:22 +0530

    Updated dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,22 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export const Dashboard = () => {
+    const [students, setStudents] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: `http://localhost:8080/student?teacher=${localStorage.getItem("umrikey")}`
+        }).then(res => {
+            setStudents(res.data)
+            console.log(res.data)
+        })
+            .catch(err => console.error(err.message))
+    })
   return (
       <div>
-          dashboard
+          dashboard boi
       </div>
   )
 }
